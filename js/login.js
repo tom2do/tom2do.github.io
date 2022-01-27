aac78c19853cc0ea0f69b0420bf746b9d09ff041
TomTom <tom@2do-digital.de>
2022-01-27 21:03:46 +0100

    Login

---
+++ js/login.js
@@ -0,0 +1,48 @@
+var login = {
+	loginform: "",
+	user: "",
+	pass: "",
+	loginWindow: "",
+	content: "",
+
+	init() { 
+		this.loginform = document.querySelector('#loginform')
+		this.loginWindow = document.querySelector('.login__window')
+		this.content = document.querySelector('.secured__content')
+		this.registerListeners()
+		this.updateView()
+	},
+
+	updateView() { 
+		if (sessionStorage.getItem('ab__login')) {
+			this.loginform.style.display = "none"
+			this.content.classList.remove('hidden')
+		} else { 
+			this.loginform.style.display = ""			
+			this.content.classList.add('hidden')
+		}
+	},
+	registerListeners() { 
+		this.loginform.addEventListener('submit', function (e) { 
+			e.preventDefault()
+			login.user = login.loginform.user.value
+			login.pass = login.loginform.pass.value
+			console.log(login.pass, login.user);
+			login.submitLogin(login.user, login.pass)
+			login.updateView()
+		})
+	},
+	submitLogin(user, pass) {
+		if (user === 'antje' && pass === "bruendlinger") {
+			return sessionStorage.setItem('ab__login', true)
+		} 
+		return sessionStorage.setItem('ab__login', false)
+	},
+
+}
+window.addEventListener('DOMContentLoaded', function () { 
+	login.init()
+	
+	
+})
+
